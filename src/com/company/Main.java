378f1baedb81fb505ba877d8c04b44729f8553e9
Marwa Mubarak <marwamubarak6@gmail.com>
2022-11-13 17:09:53 +0200

    format the code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,80 +18,73 @@ public class Main {
         return ans;
     }
 
-    static void Compression()
-    {
-        HashMap<String,Integer>dic=new HashMap<String,Integer>();
-        for(int i=0;i<128;i++)
-        {
-            dic.put((char)i+"",i);
+    static void Compression() {
+        HashMap<String, Integer> dic = new HashMap<String, Integer>();
+        for (int i = 0; i < 128; i++) {
+            dic.put((char) i + "", i);
         }
         System.out.println("Enter Text to Compression: ");
         Scanner input = new Scanner(System.in);
-        String str=input.next();
-        Vector<Integer>Compression=new Vector<>();
-        String curr="";
-        int cnt=128;
-        int mx=0;
-        for(int i=0;i<str.length();i++)
-        {
-            curr+=str.charAt(i);
-            if(!dic.containsKey(curr))
-            {
-                dic.put(curr,cnt++);
-                curr=curr.substring(0,curr.length() - 1);
+        String str = input.next();
+        Vector<Integer> Compression = new Vector<>();
+        String curr = "";
+        int cnt = 128;
+        int mx = 0;
+        for (int i = 0; i < str.length(); i++) {
+            curr += str.charAt(i);
+            if (!dic.containsKey(curr)) {
+                dic.put(curr, cnt++);
+                curr = curr.substring(0, curr.length() - 1);
                 Compression.add(dic.get(curr));
-                mx=Math.max(mx,dic.get(curr));
-                curr="";
+                mx = Math.max(mx, dic.get(curr));
+                curr = "";
                 i--;
             }
         }
-        if(curr.length()>0)
-        {
+        if (curr.length() > 0) {
             Compression.add(dic.get(curr));
-            mx=Math.max(mx,dic.get(curr));
+            mx = Math.max(mx, dic.get(curr));
 
         }
-        for(int i=0;i<Compression.size();i++)
-        {
-            System.out.print("<"+ Compression.get(i) + "> " );
+        for (int i = 0; i < Compression.size(); i++) {
+            System.out.println("<" + Compression.get(i) + "> ");
         }
         System.out.println();
-        int after=NumOfBits(mx)*Compression.size();
-        int before=str.length()*8;
-        System.out.println("THe Size Before Compression = " + before+ " Bits");
+        int after = NumOfBits(mx) * Compression.size();
+        int before = str.length() * 8;
+        System.out.println("THe Size Before Compression = " + before + " Bits");
         System.out.println("THe Size After Compression = " + after + " Bits");
     }
 
-    static void Decompression(){
+    static void Decompression() {
         Scanner input = new Scanner(System.in);
-        ArrayList<Integer>tags = new ArrayList<Integer>();
+        ArrayList<Integer> tags = new ArrayList<Integer>();
         System.out.println("Enter Tags Size:");
         int tagsSize = input.nextInt();
-        System.out.println("Enter "+tagsSize+" Tags:");
-        for (int i=0;i<tagsSize;i++){
-            int x=input.nextInt();
+        System.out.println("Enter " + tagsSize + " Tags:");
+        for (int i = 0; i < tagsSize; i++) {
+            int x = input.nextInt();
             tags.add(x);
         }
 
-        String decompressionTxt="";
-        HashMap<Integer,String>dic=new HashMap<Integer,String>();
-        for(int i=0;i<128;i++)
-        {
-            dic.put(i,(char)i+"");
+        String decompressionTxt = "";
+        HashMap<Integer, String> dic = new HashMap<Integer, String>();
+        for (int i = 0; i < 128; i++) {
+            dic.put(i, (char) i + "");
         }
-        int curr=128;
-        String prev="";
-        for (int i=0;i<tags.size();i++){
-            if(i>0){
-                dic.put(curr,prev);
-                dic.put(curr, prev +dic.get(tags.get(i)).charAt(0));
+        int curr = 128;
+        String prev = "";
+        for (int i = 0; i < tags.size(); i++) {
+            if (i > 0) {
+                dic.put(curr, prev);
+                dic.put(curr, prev + dic.get(tags.get(i)).charAt(0));
                 curr++;
             }
-            decompressionTxt+=dic.get(tags.get(i));
-            prev=dic.get(tags.get(i));
+            decompressionTxt += dic.get(tags.get(i));
+            prev = dic.get(tags.get(i));
 
         }
-        System.out.println("The Decompression Text: "+decompressionTxt);
+        System.out.println("The Decompression Text: " + decompressionTxt);
 
 
     }
@@ -113,18 +106,13 @@ public class Main {
             int choice = input.nextInt();
             System.out.println("-------------------------------------");
 
-            if (choice == 1)
-            {
+            if (choice == 1) {
                 Compression();
                 System.out.println("-------------------------------------");
-            }
-            else if (choice == 2)
-            {
+            } else if (choice == 2) {
                 Decompression();
                 System.out.println("-------------------------------------");
-            }
-            else if (choice == 3)
-            {
+            } else if (choice == 3) {
                 System.out.println("Exit..");
                 System.out.println("-------------------------------------");
                 break;
@@ -135,24 +123,22 @@ public class Main {
 }
 
 /**
-
- 14
-
- 65
- 66
- 65
- 128
- 128
- 129
- 131
- 134
- 130
- 129
- 66
- 138
- 139
- 138
-
- ABAABABBAABAABAAAABABBBBBBBB
-
+ * 14
+ * <p>
+ * 65
+ * 66
+ * 65
+ * 128
+ * 128
+ * 129
+ * 131
+ * 134
+ * 130
+ * 129
+ * 66
+ * 138
+ * 139
+ * 138
+ * <p>
+ * ABAABABBAABAABAAAABABBBBBBBB
  **/
